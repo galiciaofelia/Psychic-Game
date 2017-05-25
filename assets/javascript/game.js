818e493302f6be6c0d74f4a7081138a94751928c
user <user@MacBook-Air.local>
2017-05-24 22:30:45 -0700

    updated game.js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,16 +2,18 @@
 
 
 
+
 var alphabetLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w","x","y","z"];
 
-// Variables for wins, losses and ties
-    var wins = 0;
-    var losses = 0;
-    var guessesLeft = 3;
-    var guessesSoFar = [];
+
+// Variables for wins, losses
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var guessesSoFar = [];
 
 //computer chooses a random letter:
-    var computerChoice = alphabetLetters[Math.floor(Math.random() * alphabetLetters.length)];
+var computerChoice = alphabetLetters[Math.floor(Math.random() * alphabetLetters.length)];
 
    // When the user presses a key, it will run the following function...
 document.onkeypress = function(event) {
@@ -19,14 +21,29 @@ document.onkeypress = function(event) {
 
     if(userGuess === computerChoice){
         wins++;
+        alert("You won!");
+        //Need to reset guessesLeft to 9
+        guessesLeft =  9;
+        //Reset to fresh start guessesSoFar
+        guessesSoFar = [];
+
     }
 
     else{
-        guessesLeft--;
+        if(guessesSoFar.indexOf(userGuess) === -1) {
+            guessesSoFar.push(userGuess);
+            guessesLeft--;       
+        }
     }
 
     if(guessesLeft === 0){
         losses++;
+        alert("You lost!");
+        //I need to reset guessesLeft to 9
+        guessesLeft = 9;
+        //Reset to fresh start guessesSoFar
+        guessesSoFar = [];
+
     }
 
     document.getElementById("wins").innerHTML = "Wins: " + wins;
